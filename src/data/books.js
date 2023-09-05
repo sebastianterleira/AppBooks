18ef1016fc12eb56d5c3b42dca75bf14d574e2fd
sebastianterleira <seb.terleira1204@gmail.com>
2023-09-04 21:39:21 -0500

    MOD: Actualice los datos de los books

--- src/data/books.js
+++ src/data/books.js
@@ -1,22 +1,31 @@
 export const books = [
   {
-    id: 'clear-code',
-    title: 'Clean Code: Manual de desarrollo ágil de software',
-    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+    id: "clear-code",
+    title: "Clean Code: Manual de desarrollo ágil de software",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg",
+    opinion:
+      "lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet",
+    author: "Robert C. Martin",
   },
   {
-    id: 'the-clean-coder',
-    title: 'The Clean Coder: A code of Conduct for Proffessional Programmers',
-    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+    id: "the-clean-coder",
+    title: "The Clean Coder: A code of Conduct for Proffessional Programmers",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg",
+    opinion: "lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet",
+    author: "Robert C. Martin",
   },
   {
-    id: 'javascript-the-good-parts',
-    title: 'JavaScript: The Good Parts',
-    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+    id: "javascript-the-good-parts",
+    title: "JavaScript: The Good Parts",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg",
+    opinion: "lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet",
+    author: "Douglas Crockford",
   },
   {
-    id: 'eloquent-javascript',
-    title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
-  }
-]
+    id: "eloquent-javascript",
+    title: "Eloquent JavaScript: A Modern Introduction to Programming",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg",
+    opinion: "lorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit ametlorem ipsum dolor sit amet",
+    author: "Marijn Haverbeke",
+  },
+];
